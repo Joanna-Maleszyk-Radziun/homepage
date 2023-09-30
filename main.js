3fee404d2be4748b35d15cff9f50c0754bb47be8
asia maleszyk-radziun <artmaleszyk@gmail.com>
2023-09-30 17:26:46 +0200

    DOM

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const firstName = 'Maciek';
+const firstName = 'Maciek Korsan';
 const age = 35;
 
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat`)
+
+const header = document.querySelector('.page-header__heading--js')
+
+header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`
+
+console.log(header.style)
+
+header.style.color = 'red';
