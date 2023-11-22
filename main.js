a3524aef531a552ff250531e21eef6bdd972d288
asia maleszyk-radziun <artmaleszyk@gmail.com>
2023-11-22 14:42:38 +0100

    repeted BEM&DOM

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
 const firstName = 'Maciek Korsan';
 const age = 35;
 
-
 console.log(`Nazywam się ${firstName} i mam ${age} lat`)
 
 const header = document.querySelector('.page-header__heading--js')
-
+ 
 header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`
 
 console.log(header.style)
 
-header.style.color = 'red';
-
-const contentJs = document.querySelector('.my-article__content--js');
-
-contentJs.innerHTML = 'Jestem wypełniony javascriptem 😁'
+header.style.color = 'red'
