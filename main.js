6dbbbaafbfec2738a215a4f55f03d7ad5af3212e
asia maleszyk-radziun <artmaleszyk@gmail.com>
2023-12-18 15:00:39 +0100

    repatition MO4, typography MO5

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ console.log(`Nazywam się ${firstName} i mam ${age} lat`)
 
 const header = document.querySelector('.page-header__heading--js')
  
-header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`
 
 console.log(header.style)
 
 header.style.color = 'red'
+
+const contentJs = document.querySelector('.my-article__content--js')
+
+contentJs.innerHTML = 'Jestem wypełniony javascriptem 🤯'
