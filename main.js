121c485b181bcac769fc8aa5f129f72ac6a6da46
asia maleszyk-radziun <artmaleszyk@gmail.com>
2024-03-05 17:35:46 +0100

    querySelector exercise

--- main.js
+++ main.js
@@ -5,8 +5,22 @@ console.log(`Nazywam się ${firstName} i mam ${age} lat`)
 
 const header = document.querySelector('.page-header__heading--js')
 
- header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`
+console.log(header.innerHTML)
 
- console.log = (header.style)
+header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`
 
- header.style.color = `red`
+console.log(header.style)
+
+header.style.color = 'red'
+
+const contentJs = document.querySelector('.my-article__content--js')
+
+contentJs.innerHTML = 'Jestem wypełniony JS🤯!'
+
+const tekst = document.querySelector('.my-article__tekst--js')
+
+tekst.innerHTML = 'ciąg dalszy ćwiczeń'
+
+const gora = document.querySelector('.my-article__gora--js')
+
+gora.innerHTML = 'ćwiczę sobie querySelector'
