37aed5d83bc986788d272fe4c9b63ed04ea591bc
Joanna-Maleszyk-Radziun <artmaleszyk@gmial.com>
2024-07-08 13:58:10 +0200

    add click - event

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const gora = document.querySelector('.my-article__gora--js')
 
 gora.innerHTML = 'jfndfvbfhvbr'
 
-const calculate = myNumber => (myNumber+4)*7;
+const calculate = myNumber => (myNumber + 4) * 7;
 
 calculate(2);
 
@@ -36,7 +36,7 @@ hello();
 const deathStar = {
     diameter: 120000,
     fire: (target) => {
-    console.log(`${target} destroyed 💥`)
+        console.log(`${target} destroyed 💥`)
     },
     isOperating: true,
     levels: 357,
@@ -45,20 +45,27 @@ const deathStar = {
     commander: {
         name: 'Darth Vader'
     }
-    }
+}
 
-    console.log(deathStar.commander.name)
-    console.log(deathStar['levels'])
-    deathStar.fire('alderaan')
-    
+console.log(deathStar.commander.name)
+console.log(deathStar['levels'])
+deathStar.fire('alderaan')
 
-    
-    const greet = (name, age) => {
-        const result = `Witaj ${name}! Masz ${age} lat.`;
-        return result;
-    }
 
-    const greeting = greet('Maciek', 35);
-    console.log(greeting);
-    console.log(greet('Asia', 85));
-    
+
+const greet = (name, age) => {
+    const result = `Witaj ${name}! Masz ${age} lat.`;
+    return result;
+}
+
+const greeting = greet('Maciek', 35);
+console.log(greeting);
+console.log(greet('Asia', 85));
+
+
+const button = document.querySelector('.page-header__button--js')
+
+button.addEventListener('click', (e) =>{
+    const header = document.querySelector('.page-header__heading--js')
+    header.innerHTML = 'klik, klik';
+})
