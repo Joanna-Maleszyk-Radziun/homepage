e1030df2743b2b9a4e870a808418a7c12bd00e43
Joanna-Maleszyk-Radziun <artmaleszyk@gmial.com>
2024-09-02 14:07:02 +0200

    added arrays

---
+++ src/js/arrays.js
@@ -0,0 +1,19 @@
+const hardware = ['mouse', 'keyboard', 'screen',]
+
+const newHardware = hardware.slice(0,2);
+
+console.log(newHardware);
+
+
+const myObject = {
+    name: "Maciek",
+    age: 35
+}
+
+
+hardware.push(myObject);
+
+console.log(hardware);
+
+const myText = "tu przykładowy string"
+console.log(myText.slice(0,7));
